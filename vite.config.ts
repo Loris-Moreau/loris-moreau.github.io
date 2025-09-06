888f815e92ad112021ac46d16547b14d2e90c0d1
Loris M. <80474693+Loris-Moreau@users.noreply.github.com>
2025-09-06 10:30:06 +0200

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import React from 'react'
+import ReactPlayer from 'react-player'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
